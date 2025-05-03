ff92e581b424b1bbb2c1999be895b5a67df75a31
Complexadious <prouljac002@gmail.com>
2025-05-02 18:13:24 -0600

    Initial Commit

---
+++ save.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name SaveData
+
+@export var saveunlockedlevels: int
+@export var savename: String
+@export var beatenlevels: int
+
+@export var mastervol: float
+@export var musicvol: float
+@export var fullscreen: bool
+@export var physics_cull_amt: float
+
+@export var lighting: bool
+@export var physics_cull: bool
+@export var enable_physics_sphere: bool
+@export var debug_rendering: bool
+
+@export var enable_sheep_targetting: bool

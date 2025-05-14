2a5db8c1cf5bcfb13355ca4ba9bf6e62490db648
Complexadious <prouljac002@gmail.com>
2025-05-14 12:33:21 -0600

    Removed kade

--- docs/CrazyCattle3D.service.worker.js
+++ docs/CrazyCattle3D.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747247210|383342132';
+const CACHE_VERSION = '1747247593|762385344';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
